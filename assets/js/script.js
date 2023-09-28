5f6d5930baae03c09d3874a8e263de39d3bc5b42
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-09-27 23:13:49 -0400

    Made prompt variables inside js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var length = prompt("How long do you want your password to be? Enter a number 8-128")
+
+    if (length < 8) {
+      alert("Please try again with a number between 8-128")
+    }
+
+  var lowercase = confirm("Do you want to include lowercase letters? Okay for yes, Cancel for no")
+
+  var uppercase = confirm("Do you want to include uppercase letters? Okay for yes, Cancel for no")
+
+  var numeric = confirm("Do you want to include numbers? Okay for yes, Cancel for no")
+
+  var specialCharacter = confirm("Do you want to include special characters? Okay for yes, Cancel for no")
+
+  console.log(length)
+
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +34,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
