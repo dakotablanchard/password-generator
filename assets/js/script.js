d06a96acb2ad656f28fafa1548e8403f0c10514a
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-09-29 21:07:17 -0400

    wrote function for user input, and function to generate random password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,20 @@
+var userLength = 0
+var choice = []
+
+var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var spChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "~"]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(length, isLower, isUpper, isNumeric, isCharacter) { 
-  // The variable 'password' stores this function, so this function needs to return what is going to be printed
-
-  var userLength = prompt("How long do you want your password to be? Enter a number 8-128")
-
-  var userLower = confirm("Do you want to include lowercase letters? Okay for yes, Cancel for no")
-
-  var userUpper = confirm("Do you want to include uppercase letters? Okay for yes, Cancel for no")
-
-  var userNumeric = confirm("Do you want to include numbers? Okay for yes, Cancel for no")
-
-  var userSpChar = confirm("Do you want to include special characters? Okay for yes, Cancel for no")
-  
-  var lowerCharArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  var upperCharArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var spCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "~"]
-
-// if userLower, choose random index from lowerCharArray
-// if userUpper, choose random index from upperCharArray
-// if userNumeric, choose random index from userNumeric
-// if userSpChar, choose random index from spCharArra
-
-  return "New Password" /* This is where value containing generated password will go */ ; 
-} 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);  //When button is clicked, the function writePassword() is ran
 
 // Write password to the #password input
-
 function writePassword() {
+  userInput()
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); // What gets printed in the box
   
@@ -37,6 +22,53 @@ function writePassword() {
   
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() { 
+  // The variable 'password' stores this function, so this function needs to return what is going to be printed
+  var newPass = ""
+  userLength = 8
+  for (var i = 0; i < userLength; i++) {
+
+    var randomIndex = Math.floor(Math.random() * choice.length);
+    newPass = newPass + choice[randomIndex];
+  }
+
+
+
+  return newPass  /* This is where value containing generated password will go */ ; 
+} 
+
+
+function userInput() {
+
+  var userLength = prompt("How long do you want your password to be? Enter a number 8-128") // This will get a number from the user
+  var choice = [];
+    if (userLength < 8 || userLength > 128) {
+      alert("Character length invalid.")
+      return false;
+    } 
+
+    if (confirm("Do you want to include lowercase letters? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
+      choice = choice.concat(lowerChar);
+    }
+
+    if (confirm("Do you want to include uppercase letters? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
+      choice = choice.concat(upperChar);
+    }
+  
+    if (confirm("Do you want to include numbers? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
+      choice = choice.concat(numbers);
+    }
+  
+    if (confirm("Do you want to include special characters? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
+      choice = choice.concat(spChar);
+    }
+
+}
+
+
+
+
+
+
+
 
