973c0b906043cd9572f7c3017717ccf23abb30de
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-10-01 16:33:57 -0400

    commented out code, updated README

--- README.md
+++ README.md
@@ -1,32 +1,50 @@
-## User Story
+# Random Password Generator
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+The purpose of this application is to generate strong and secure passwords for anybody who needs to protect important information. <br>
+The passwords generated through this application are completely randomized with a nearly infinite amount of possible outcomes. <br>
+No passwords generated through this site are stored in any way to ensure your protection.
 
-## Acceptance Criteria
+To launch the Random Password Generator, click [here](https://dakotablanchard.github.io/password-generator/)
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+By following the link, you will be taken to a website that looks like the following image:
+
+![Screenshot of password generator](./assets/images/liveSite.png)
+
+## How to use
+
+<ul>
+<li>Step 1: Click on the red 'Generate Password' button.</li>
+
+<li>Step 2: Once you have clicked the button, answer the following prompts that will appear on the screen:</li>
+    <ol>
+     <li>"How long do you want your password to be? Enter a number 8-128"</li>
+     <li>"Do you want to include lowercase letters? Okay for yes, Cancel for no"</li>
+     <li>"Do you want to include uppercase letters? Okay for yes, Cancel for no"</li>
+     <li>"Do you want to include numbers? Okay for yes, Cancel for no"</li>
+     <li>"Do you want to include special characters? Okay for yes, Cancel for no"</li>
+    </ol>
+
+<li>Step 3: Once all prompts are answered correctly, your newly generated password will appear in the text box.</li>
+</ul>
+
 
 
 ## Credits
 
-Pop Up Prompts In JavaScript https://www.w3schools.com/js/js_popup.asp
+[Pop up prompts in JavaScript](https://www.w3schools.com/js/js_popup.asp)
+
+[For loops](https://www.w3schools.com/js/js_loop_for.asp)
+
+<u>Videos watched on building a random password generator:</u>
+
+[How to Generate a RANDOM STRING in JavaScript and HTML](https://www.youtube.com/watch?v=jww3V2fSQyg&t=234s)
+
+[Basic JavaScript #13: alert/prompt/confirm](https://www.youtube.com/watch?v=ZmRYan3lAIo)
+
+[How To Generate a Random Password Using JavaScript | Password Generator JavaScript Project](https://www.youtube.com/watch?v=Xrsb9SiF3a8&t=1051s)
+
+[Random Password Generator in JavaScript](https://www.youtube.com/watch?v=v2jfGo7ztm8&t=1244s)
+
+[Random Password Generator in JavaScript: Beginner Tutorial](https://www.youtube.com/watch?v=m5XF1OQ6rSg&t=232s)
+
+[Build A Password Generator With JavaScript - Tutorial](https://www.youtube.com/watch?v=iKo9pDKKHnc)
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,51 +14,50 @@ generateBtn.addEventListener("click", writePassword);  //When button is clicked,
 
 // Write password to the #password input
 function writePassword() {
-  console.log("Write password:", userLength)
-  userInput()
-  console.log("After user input and write password", userLength)
-  var password = generatePassword(); // The variable 'password' stores this function, so this function needs to return what is going to be printed
-  var passwordText = document.querySelector("#password"); // What gets printed in the box
+
+  userInput() // Gathers input from the user
+  var password = generatePassword(); 
+  var passwordText = document.querySelector("#password");
   
-  passwordText.value = password; // What gets printed in the box is a value of the variable 'password' 
+  passwordText.value = password; // What gets printed in the box is the value of the variable 'password' 
   
 }
 
-function generatePassword() { 
+function generatePassword() { // The variable 'password' stores this function, so this function needs to return what is going to be printed
 
   var newPass = ""
-  for (var i = 0; i < userLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choice.length);
-    newPass = newPass + choice[randomIndex];
+  for (var i = 0; i < userLength; i++) { // This loop will repeat until the variable 'newPass' contains same the amount of characters selected by the user in the variable 'userLength'
+    var randomIndex = Math.floor(Math.random() * choice.length); // Chooses a random number between 0 and the length of the 'choice' array, stores number in the variable 'randomIndex'
+    newPass = newPass + choice[randomIndex]; // Adds a new value to the variable 'newPass' by taking the randomly generated number and using it as the index selector for the choice array
   }
 
-  return newPass  /* This is where value containing generated password will go */ ; 
+  return newPass  /* Value containing newly generated password */ ; 
 } 
 
 
 function userInput() {
 
-    userLength = prompt("How long do you want your password to be? Enter a number 8-128") // This will get a number from the user
+    userLength = prompt("How long do you want your password to be? Enter a number 8-128") // This will get a number from the user and store it in the variable 'userLength'
 
-    if (userLength < 8 || userLength > 128) {
+    if (userLength < 8 || userLength > 128) { // Makes it so the user can only input a value between 8-128
       alert("Character length invalid.")
       return false;
     } 
 
     if (confirm("Do you want to include lowercase letters? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
-      choice = choice.concat(lowerChar);
+      choice = choice.concat(lowerChar); // If true, this concatenates the 'lowerChar' array with the 'choice' array 
     }
 
     if (confirm("Do you want to include uppercase letters? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
-      choice = choice.concat(upperChar);
+      choice = choice.concat(upperChar); // If true, this concatenates the 'upperChar' array with the 'choice' array 
     }
   
     if (confirm("Do you want to include numbers? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
-      choice = choice.concat(numbers);
+      choice = choice.concat(numbers); // If true, this concatenates the 'numbers' array with the 'choice' array 
     }
   
     if (confirm("Do you want to include special characters? Okay for yes, Cancel for no")) { // This will get a boolean value from the user
-      choice = choice.concat(spChar);
+      choice = choice.concat(spChar); // If true, this concatenates the 'spChar' array with the 'choice' array 
     }
 }
 
